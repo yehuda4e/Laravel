9a0bc690eb1a4fc63cb5c164a4eb34f7e08925df
Yehuda Shamayev <yehuda4e@gmail.com>
2016-12-07 18:16:01 +0200

    add fillable fields

--- app/Group.php
+++ app/Group.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Group extends Model
 {
+	protected $fillable = ['name', 'color', 'permissions'];
     public $timestamps = false;
 
     public function user() {
